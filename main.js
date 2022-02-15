0142a930d8a08706a81fe7a589103138e0dbce0c
Abbi Engel <abbi.engel33@gmail.com>
2022-02-15 13:10:40 -0600

    grades

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-function main() {
-    const myMap = new Map();
+import input from 'input';
 
-    const pets = new Map();
-    pets.set('Otis', 'saint bernard');
-    pets.set('Rocko', 'german shepard');
-    pets.forEach((breed, name) => {
-        console.log(`${name} is a ${breed} and is a good boy.`)
-    });
+async function prompt(grades) {
+    while (true) {
+        const resp = await input.text('Students name (or cancel)');
+    if (resp == 'cancel') {
+        break;
+    } else {
+        const grade = await input.text('Students Grade');
+        grades.set(resp, grade);
+    }
+}
+}
 
+function display(grades) {
+    for (const [name, grade] of grades) {
+        console.log(`${name} has a ${grade}%`); 
+    }
+}
+
+async function main() {
+    const grades = new Map();
+
+    await prompt(grades);
+    display(grades);
 }
 
 main();
