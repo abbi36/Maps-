fa77a9e3abe3442e7c0d13a96b5ad078aeaa2722
Abbi Engel <abbi.engel33@gmail.com>
2022-02-15 14:18:00 -0600

    car details

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 import input from 'input';
 
-async function prompt(grades) {
+async function prompt(cars) {
     while (true) {
-        const resp = await input.text('Students name (or cancel)');
-    if (resp == 'cancel') {
-        break;
-    } else {
-        const grade = await input.text('Students Grade');
-        grades.set(resp, grade);
+        const name= await input.text('What is your name?');
+        const specs = {
+        make: await input.text('What is the make of your car?'),
+        model: await input.text('What model is your car?'),
+        year: await input.text('What year is your car?'),
+    }
+    cars.set(name,specs);
+    const end = await input.text('Are you finished?');
+        if (end == 'yes'){
+            break;
+        }
     }
 }
-}
 
-function display(grades) {
-    for (const [name, grade] of grades) {
-        console.log(`${name} has a ${grade}%`); 
+function display(cars) {
+    for (const[name, specs] of cars) {
+        console.log(`${name} has a ${specs.year}, ${specs.make}, ${specs.model}.`);
     }
 }
 
-async function main() {
-    const grades = new Map();
-
-    await prompt(grades);
-    display(grades);
+async function  main() {
+    const myCars = new Map();
+    await prompt(myCars);
+    display(myCars);
 }
+
 
 main();
